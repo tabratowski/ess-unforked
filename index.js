7df212aa79aeaae84722db1fdddef1790c93a565
Tomasz Abratowski <tomasz.abratowski@wpengine.com>
2023-06-13 08:51:00 +0200

    Revert "dsfgdfghdf"
    This reverts commit b1f2c190cde33104c88378321af744f256624181.
    
    # Conflicts:
    #	index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 3300;
 server.use(express.static('public'));
 
 server.get('/', (_req, res) => {
+  res.send('Hello Express ver 2.0! kalafior!');
   res.send('Hello Express!');
 });
 
